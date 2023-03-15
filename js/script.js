323d3b899480ad3d30dc04bf653cbc1b6f4e1597
askatdev <askatabdirashitov3@gmail.com>
2023-03-15 10:26:43 +0600

    first

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const themeBtn = document.getElementById('theme');
+
+themeBtn.addEventListener('click', () => {
+    themeBtn.style.rotate = '180deg';
+    document.body.classList.toggle('dark');
+});
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
